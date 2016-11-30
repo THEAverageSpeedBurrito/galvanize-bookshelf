69e9d2604685e0df077ee1d93b5eca30e19c376c
Evan Busse <evanbusse@gmail.com>
2016-11-30 16:29:24 -0700

    Able to post, unable to hash

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,27 @@
 'use strict';
 
 const express = require('express');
+const knex = require('../knex');
+const bodyParser = require('body-parser');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
+router.use(bodyParser.json());
 // YOUR CODE HERE
+
+router.post('/', (req, res, next) => {
+  var {first_name, last_name, email, hashed_password} = req.body;
+
+  knex('users').insert({
+    first_name,
+    last_name,
+    email,
+    hashed_password
+  }).then((users) => {
+    res.send(users[0]);
+  });
+
+});
 
 module.exports = router;

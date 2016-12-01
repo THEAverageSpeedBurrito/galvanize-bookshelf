89787013e98f91e95e160a5a475c05da18159a4a
Evan Busse <evanbusse@gmail.com>
2016-12-01 13:49:38 -0700

    added hashing function

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const knex = require('../knex');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-as-promised');
+const {camelizeKeys, decamelizeKeys} = require('humps');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
@@ -11,17 +13,37 @@ router.use(bodyParser.json());
 // YOUR CODE HERE
 
 router.post('/', (req, res, next) => {
-  var {first_name, last_name, email, hashed_password} = req.body;
+  var {firstName, lastName, email, password} = req.body;
 
-  knex('users').insert({
-    first_name,
-    last_name,
-    email,
-    hashed_password
-  }).then((users) => {
-    res.send(users[0]);
+  bcrypt.hash(password, 5).then((hashed_password) => {
+
+    knex('users').insert({
+      first_name: firstName,
+      last_name: lastName,
+      email,
+      hashed_password
+    }, '*').then((users) => {
+      delete users[0].hashed_password;
+      delete users[0].created_at;
+      delete users[0].updated_at;
+      res.send(camelizeKeys(users[0]));
+    });
+
   });
+
 
 });
 
 module.exports = router;
+
+
+// router.post('/users', (req, res, next) => {
+//   bcrypt.hash(req.body.password, 12)
+//     .then((hashed_password) => {
+//       console.log(req.body.email, hashed_password);
+//       res.sendStatus(200);
+//     })
+//     .catch((err) => {
+//       next(err);
+//     });
+// });
